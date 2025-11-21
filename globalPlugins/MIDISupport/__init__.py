c5ecfa563c0951e0251c1f21e245304ee0ce2765
Dalen <dbernaca@gmail.com>
2025-11-21 03:55:49 +0100

    * Skeleton working POC (Clashes with Object Location Tones - one of them crashes when opening same MIDI output)

--- globalPlugins/MIDISupport/__init__.py
+++ globalPlugins/MIDISupport/__init__.py
@@ -1,1 +1,24 @@
-from globalPluginHandler import GlobalPlugin
+from globalPluginHandler import GlobalPlugin as GP
+from . import midi
+from .midi import frequency_to_midi
+import tones
+import wx
+
+class GlobalPlugin (GP):
+    def __init__ (self):
+        GP.__init__(self)
+        midi.init()
+        self.output = o = midi.Output(midi.get_default_output_id())
+        self.player = midi.Player(o)
+        tones.decide_beep.register(self.intone)
+
+    def terminate (self):
+        tones.decide_beep.unregister(self.intone)
+        self.player.quit()
+        midi.quit()
+
+    def intone (self, hz, length, left, right, isSpeechBeepCommand):
+        self.player.pan(left, right)
+        self.player.play(frequency_to_midi(hz), length)
+        wx.CallLater(length, self.player.tick)
+        return False
